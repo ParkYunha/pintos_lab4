1d558fbb78c407bfe49037f00d854407f49527e2
박윤하 <yunha@bag-yunhaui-MacBook-Pro.local>
2019-05-25 18:19:50 +0900

    지저분한거 정리

---
+++ src/filesys/build/filesys/cache.d
@@ -0,0 +1,7 @@
+filesys/cache.o: ../../filesys/cache.c ../../lib/debug.h \
+ ../../lib/string.h ../../lib/stddef.h ../../lib/kernel/list.h \
+ ../../lib/stdbool.h ../../lib/stdint.h ../../filesys/cache.h \
+ ../../filesys/off_t.h ../../filesys/filesys.h ../../devices/disk.h \
+ ../../lib/inttypes.h ../../threads/synch.h ../../threads/palloc.h \
+ ../../threads/malloc.h ../../threads/thread.h ../../threads/synch.h \
+ ../../devices/timer.h ../../lib/round.h
